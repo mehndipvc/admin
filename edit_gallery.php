ae53390cb0ab9339245a9cc0337e3854ec172a29
mehndipvc <mehndipvc@gmail.com>
2025-06-16 22:43:48 +0530

    Update edit_gallery.php

--- edit_gallery.php
+++ edit_gallery.php
@@ -1,76 +1,66 @@
 <?php
 include("config.php");
 include("add-watermark.php");
-// print_r($_POST);
-// exit;
-if (!empty($_POST['status']) && !empty($_POST['id']) && !empty($_POST['cat_id'])) {
+
+if (!empty($_POST['status']) && !empty($_POST['id'])) {
     $status = $_POST['status'];
     $id = $_POST['id'];
-    $cat_id = $_POST['cat_id'];
     $old_image = $_POST['old_image'];
 
+    $filenameToUpdate = $old_image; // Default to old image
+    $hasNewImage = false;
 
+    // Check if a new image is uploaded
     if (!empty($_FILES["image_new"]['name'][0])) {
         $files = $_FILES["image_new"];
         $allowed = array('png', 'jpg', 'jpeg', 'webp');
         $validImageNames = array();
 
-
-        $invalidFileFound = false;
-
         for ($i = 0; $i < count($files['name']); $i++) {
-            $doc = $files['name'][$i];
-            $ext = pathinfo($doc, PATHINFO_EXTENSION);
-
+            $ext = pathinfo($files['name'][$i], PATHINFO_EXTENSION);
             if (!in_array($ext, $allowed)) {
-                $invalidFileFound = true;
-                break;
+                echo "Invalid file format. Allowed formats: " . implode(', ', $allowed);
+                exit;
             }
         }
 
-        if ($invalidFileFound) {
-            echo "Invalid file format. Allowed formats: " . implode(', ', $allowed) . "<br>";
-            exit;
-        }
-
         for ($i = 0; $i < count($files['name']); $i++) {
-            $doc = $files['name'][$i];
             $tmp = $files['tmp_name'][$i];
-            $temp = explode(".", $doc);
-            $newfile = rand(0000000000000, 9999999999999) . '.' . end($temp);
+            $temp = explode(".", $files['name'][$i]);
+            $newfile = rand(1000000000, 9999999999) . '.' . end($temp);
             $folder = "../api/assets/" . $newfile;
 
             move_uploaded_file($tmp, $folder);
             addWatermark($folder);
             $validImageNames[] = $newfile;
         }
-        $validImageNamesString = implode(', ', $validImageNames);
 
-        $oldImageNamesArray = explode(', ', $old_image);
-        foreach ($oldImageNamesArray as $oldImageName) {
-            $imageFilePath = "../api/assets/" . $oldImageName;
-            unlink($imageFilePath);
+        // Delete old image(s)
+        foreach (explode(', ', $old_image) as $img) {
+            $imgPath = "../api/assets/" . $img;
+            if (file_exists($imgPath)) {
+                unlink($imgPath);
+            }
         }
+
+        $filenameToUpdate = implode(', ', $validImageNames);
+        $hasNewImage = true;
     }
-    $check = $obj->num("SELECT id FROM items_images WHERE id='$id'");
-    if ($check != 0) {
-        $gallery_code = rand(00000, 99999);
-        if (!empty($_FILES["image_new"]['name'][0])) {
-            $update = $obj->query("UPDATE items_images SET gallery_code='$gallery_code',status='$status',cat_id='$cat_id',filename='$validImageNamesString' WHERE id='$id'");
-            if ($update) {
-                echo '<p class="alert alert-success">Successfully Updated</p>';
-            }
-        } else {
 
-            $update = $obj->query("UPDATE items_images SET gallery_code='$gallery_code',status='$status',cat_id='$cat_id',filename='$old_image' WHERE id='$id'");
-            if ($update) {
-                echo '<p class="alert alert-success">Successfully Updated</p>';
-            }
-        }
+    $gallery_code = rand(10000, 99999);
+    $sql = "UPDATE items_images SET 
+                gallery_code = '$gallery_code',
+                status = '$status',
+                filename = '$filenameToUpdate' 
+            WHERE id = '$id'";
+
+    $update = $obj->query($sql);
+
+    if ($update) {
+        echo '<p class="alert alert-success">Successfully Updated</p>';
     } else {
-        echo '<p class="alert alert-danger">Error Faild Submission</p>';
+        echo '<p class="alert alert-danger">Failed to update record.</p>';
     }
 } else {
     echo '<p class="alert alert-danger">Empty Field</p>';
 }
-?>
